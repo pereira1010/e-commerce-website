4399815111ac9b8a7adaa0c0ecda87f0a6c2db48
juanrangel43 <42121460+juanrangel43@users.noreply.github.com>
2024-12-17 12:28:28 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const path = require('path');
 const staticRoutes = require('./js/staticPages.js');
+const dbRoutes = require('./db/database.js');
 
 const app = express();
 const port = 3000;
 
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(staticRoutes);
+app.use('/database', dbRoutes);
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
